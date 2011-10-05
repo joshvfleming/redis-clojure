e5a5f4c4d455e39ca3d482a669a45a68c00ca437
joshvfleming <jf@joshfleming.com>
2011-10-04 18:18:47 -0700

    Bumped clojure version in project deps.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject redis-clojure "1.2.4"
   :description "A client library for Redis"
   :url "http://github.com/ragnard/redis-clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [commons-pool/commons-pool "1.5.5"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
